42cd7945d6975271ad8d8d83be300ff18ffe5a54
ryangualberto <ryanoliveiragualberto@gmail.com>
2025-03-22 15:07:33 -0300

    feat: simplificar configuração de CORS removendo opções específicas

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization',
-  });
+  app.enableCors({});
   await app.listen(process.env.PORT);
 }
 bootstrap();
